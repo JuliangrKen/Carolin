735b6b4049b92c15ed08be4ce7130974ec2c6d51
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-10-13 23:47:48 +0300

    Оформил входную точку приложения

--- Carolin.Bot/Program.cs
+++ Carolin.Bot/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Carolin.Bot.Configuration;
+using Carolin.Bot.Interfaces;
+using Carolin.Bot.Services;
+using Carolin.Bot.Utilites;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+var botConfig = ConfigGettter<DiscordBotConfig>.GetFromJsonFile();
+var socketClient = new DiscordSocketClient();
+
+var services = new ServiceCollection()
+    .AddSingleton(botConfig)
+    .AddSingleton(socketClient)
+    .AddSingleton<DiscordBotService>()
+    .AddSingleton<MessageCommandsService>()
+    .AddSingleton<ILogger, ConsoleLoggerService>();
+
+var provider = services.BuildServiceProvider();
+services.AddSingleton(services.BuildServiceProvider());
+
+if (provider.GetService<DiscordBotService>() is not DiscordBotService botService)
+    throw new Exception();
+
+await botService.RunAsync();
