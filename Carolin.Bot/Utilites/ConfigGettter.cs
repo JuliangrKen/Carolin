f416b5fcaabd6d199835c3ea87928d8eb4df279a
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-10-13 23:47:20 +0300

    Добавил утилиту для получения конфига

---
+++ Carolin.Bot/Utilites/ConfigGettter.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace Carolin.Bot.Utilites
+{
+    internal static class ConfigGettter<T> where T : class
+    {
+        public static T GetFromJsonFile()
+        {
+            var path = $@"{Environment.CurrentDirectory}\Configuration\{typeof(T).Name}.json";
+            return GetFromJsonFile(path);
+        }
+
+        public static T GetFromJsonFile(string path)
+        {
+            var json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<T>(json) ?? throw new ArgumentNullException(nameof(T));
+        }
+    }
+}
