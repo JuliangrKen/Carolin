cec23f26de5bbacb123c68413f11b6971ab24ffa
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-10-14 10:45:09 +0300

    Перевел в метод (временно)

--- Carolin.Bot/Services/DiscordBotService.cs
+++ Carolin.Bot/Services/DiscordBotService.cs
@@ -1,7 +1,9 @@
 ﻿using Carolin.Bot.Configuration;
 using Carolin.Bot.Interfaces;
 using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using System.Reflection;
 
 namespace Carolin.Bot.Services
 {
@@ -9,24 +11,51 @@ namespace Carolin.Bot.Services
     {
         private readonly DiscordSocketClient socketClient;
         private readonly DiscordBotConfig botConfig;
+        private readonly CommandService commandService;
+        private readonly IServiceProvider serviceProvider;
         private readonly ILogger logger;
 
         public DiscordBotService(DiscordSocketClient socketClient, 
             DiscordBotConfig botConfig,
+            CommandService commandService,
+            IServiceProvider serviceProvider,
             ILogger logger)
         {
             this.socketClient = socketClient;
             this.botConfig = botConfig;
+            this.commandService = commandService;
+            this.serviceProvider = serviceProvider;
             this.logger = logger;
         }
 
         public async Task RunAsync()
         {
             socketClient.Log += Log;
+            socketClient.MessageReceived += CommandHandleAsync;
 
+            await commandService.AddModulesAsync(Assembly.GetEntryAssembly(), serviceProvider);
             await socketClient.LoginAsync(TokenType.Bot, botConfig.Token);
             await socketClient.StartAsync();
             await Task.Delay(-1);
+        }
+
+        private async Task CommandHandleAsync(SocketMessage msg)
+        {
+            if (msg is not SocketUserMessage message)
+                return;
+
+            int argPos = 0;
+            if (message.Author.IsBot || !message.HasStringPrefix(botConfig.Prefix, ref argPos))
+                return;
+
+            var context = new SocketCommandContext(socketClient, message);
+            var result = await commandService.ExecuteAsync(context, argPos, serviceProvider);
+
+            if (!result.IsSuccess)
+                await logger.Log(result.ErrorReason);
+
+            if (result.Error.Equals(CommandError.UnmetPrecondition))
+                await message.Channel.SendFileAsync(result.ErrorReason);
         }
 
         private Task Log(LogMessage arg) =>
