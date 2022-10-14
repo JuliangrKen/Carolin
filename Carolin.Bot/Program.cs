efe8247624f069c4fe7d1cd4f21662dccee33f30
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-10-14 10:44:54 +0300

    Поработал с конфигами

--- Carolin.Bot/Program.cs
+++ Carolin.Bot/Program.cs
@@ -2,17 +2,33 @@
 using Carolin.Bot.Interfaces;
 using Carolin.Bot.Services;
 using Carolin.Bot.Utilites;
+using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 
 var botConfig = ConfigGettter<DiscordBotConfig>.GetFromJsonFile();
-var socketClient = new DiscordSocketClient();
+
+var socketConfig = new DiscordSocketConfig()
+{
+    AlwaysDownloadUsers = true,
+    ConnectionTimeout = 10_000,
+    MessageCacheSize = 100,
+    GatewayIntents = GatewayIntents.All
+};
+var socketClient = new DiscordSocketClient(socketConfig);
+
+var commandServiceConfig = new CommandServiceConfig()
+{
+    DefaultRunMode = RunMode.Async,
+};
+var commandService = new CommandService(commandServiceConfig);
 
 var services = new ServiceCollection()
     .AddSingleton(botConfig)
     .AddSingleton(socketClient)
+    .AddSingleton(commandService)
     .AddSingleton<DiscordBotService>()
-    .AddSingleton<MessageCommandsService>()
     .AddSingleton<ILogger, ConsoleLoggerService>();
 
 var provider = services.BuildServiceProvider();
